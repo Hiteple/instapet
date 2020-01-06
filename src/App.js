52dbce0d0758e3eec95d1932bcbedad00b84622f
hiteple <hiteplegold@gmail.com>
2020-01-06 00:42:40 -0300

    Generating animation styles with styled-components dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from './styles/GlobalStyle'
+import { Logo } from './components/Logo'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
---
+++ src/styles/animation.js
@@ -0,0 +1,18 @@
+import { css, keyframes } from 'styled-components'
+
+export const fadeInKeyframes = keyframes`
+   from {
+      filter: blur(5px);
+      opacity: 0;
+   }
+   to {
+      filter: blur(0);
+      opacity: 1;
+   }
+`
+
+export const fadeIn = ({ time = '1s', type = 'ease' } = {}) => {
+  return css`
+    animation: ${time} ${fadeInKeyframes} ${type};
+  `
+}
