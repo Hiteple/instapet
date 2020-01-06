efc92f2014d2cb95aa219ffc3983c0ae0f2a65cc
hiteple <hiteplegold@gmail.com>
2020-01-06 01:57:39 -0300

    Using Intersection Observer to load articles depending on viewport

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { ImgWrapper, Image, Button } from './styles'
+import React, { useState, useEffect, useRef } from 'react'
+import { Article, ImgWrapper, Image, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Image src={src} alt='img' />
-        </ImgWrapper>
-      </a>
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='27px' />
-        {likes} likes!
-      </Button>
-    </article>
+  useEffect(
+    function () {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(ref.current)
+    },
+    [ref]
+  )
+
+  return (
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Image src={src} alt='img' />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='27px' />
+            {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
