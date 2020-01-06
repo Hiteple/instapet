286c6757786ceb6c7fa44fddfe117e43021926d3
hiteple <hiteplegold@gmail.com>
2020-01-05 21:24:42 -0300

    Eslint integrated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Cambiando el mensaje</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>Cambiando el mensaje</h1>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js"
+    filename: 'app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html"
+      template: 'src/index.html'
     })
   ],
   module: {
@@ -14,11 +14,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
     ]
   }
-};
+}
