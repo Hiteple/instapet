207ea306d5a4151de6b7cc3211bd21352b0b5b4c
hiteple <hiteplegold@gmail.com>
2020-01-07 20:23:59 -0300

    Intersection observer polyfill implemented

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,27 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Article, ImgWrapper, Image, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import React, { useState, useEffect, useRef } from "react";
+import { Article, ImgWrapper, Image, Button } from "./styles";
+import { MdFavoriteBorder } from "react-icons/md";
 
 const DEFAULT_IMAGE =
-  'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+  "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const ref = useRef(null)
-  const [show, setShow] = useState(false)
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
 
   useEffect(
-    function () {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(ref.current)
+    function() {
+      Promise.resolve(
+        typeof window.IntersectionObserver !== "undefined"
+          ? window.IntersectionObserver
+          : import("intersection-observer")
+      ).then(() => {
+        const observer = new window.IntersectionObserver(function(entries) {
+          const { isIntersecting } = entries[0];
+          if (isIntersecting) {
+            setShow(true);
+            observer.disconnect();
+          }
+        });
+        observer.observe(ref.current);
+      });
     },
     [ref]
-  )
+  );
 
   return (
     <Article ref={ref}>
@@ -29,16 +35,16 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
         <>
           <a href={`/detail/${id}`}>
             <ImgWrapper>
-              <Image src={src} alt='img' />
+              <Image src={src} alt="img" />
             </ImgWrapper>
           </a>
 
           <Button>
-            <MdFavoriteBorder size='27px' />
+            <MdFavoriteBorder size="27px" />
             {likes} likes!
           </Button>
         </>
       )}
     </Article>
-  )
-}
+  );
+};
