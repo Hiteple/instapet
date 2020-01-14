9b9b471895b819444b62159f0ff61de67586b8b3
hiteple <hiteplegold@gmail.com>
2020-01-14 01:31:40 -0300

    Context implemented and useInputValue hook created

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import Context from '../Context'
+import { UserForm } from '../components/UserForm/index'
 
-export const NotRegisteredUser = () => {
-  return <h1>NotRegisteredUser</h1>
-}
+export const NotRegisteredUser = () => (
+  <Context.Consumer>
+    {({ activateAuth }) => {
+      return <UserForm onSubmit={activateAuth} />
+    }}
+  </Context.Consumer>
+)
